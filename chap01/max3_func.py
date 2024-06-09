95ddb6beb91ad42dc782cd806d80ce21566950e3
Gwanhoo <kimgwanho0206@gmail.com>
2024-06-10 00:55:42 +0900

    code write

---
+++ chap01/max3_func.py
@@ -0,0 +1,7 @@
+def max3(a, b, c):
+    maximum = a
+    if maximum < b:
+        maximum = b
+    if maximum < c:
+        maximum = c
+    return maximum
---
+++ chap01/sum_1_to_N_while.py
@@ -0,0 +1,12 @@
+# 1부터 N까지의 합
+
+N = int(input())
+
+Sum = 0
+i = 1
+
+while i <= N:
+    Sum += i # sum + i = sum
+    i += 1
+
+print(Sum)
